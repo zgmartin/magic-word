f1f5f9c53fd96ddd246fb24316f1366559b84a11
zgmartin <zacharygilmartin@gmail.com>
2014-07-23 21:33:02 -0400

    change dir name

---
+++ include/main.h
@@ -0,0 +1,17 @@
+/*
+  Zachary Gilmartin
+  Monday, February 24, 2014
+
+  about: A fun password program from the movie Jurasic Park
+*/
+
+//password
+#define PASSWORD "nedry"
+
+//max attempt number
+#define MAX_ATTEMPT 3
+
+ 
+//checks if password is correct
+bool check_password(char*, char*);
+
